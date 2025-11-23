16b797cf06af27faee4ffbcf3a1490e173be51f1
Safat <safatrangrezzz@gmail.com>
2025-11-23 17:32:35 +0530

    ipdeq

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,28 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Time-Table');
+  time = '02:00:00'
+  dlgbox:any=false
+
+
+
+  toggle(){
+    this.dlgbox=!this.dlgbox
+    console.log('d')
+  }
+//  setnum(val:any){
+//   const cstmTime = this.time.slice(2,this.time.length)
+//    val = String(val)
+//   // const res = val(String) + this.time
+//   console.log(val+ cstmTime)
+//   this.time= val+cstmTime
+//   console.log(this.time)
+//   console.log('dd')
+//  }
+
 }
